f46e1850f8a304adecf6c23cfc2a5214bef56a75
ZnaXxx <mtzombie@mail.ru>
2025-11-22 18:40:06 +0800

    updata

--- Titul.py
+++ Titul.py
@@ -1,2 +1,86 @@
-print('Hello World')
-print('Я ебал ваш рот')
+#                            #Пример
+# a = input()
+# b = input()
+# c = input()
+# print(a)
+# print(b)
+# print(c)
+# --------------------------------------------------------------
+#                           #Пример
+# minus = '-'
+# print('aa', 'bb', 'cc', sep=minus)
+# --------------------------------------------------------------
+#                           #Пример
+# name = input()
+# a , b , c = input(), input(), input()
+# print(a , b , c, sep=name)
+# --------------------------------------------------------------
+#                           #Пример
+# name, surname = 'Timur', 'Guev'
+# print('Имя:', name, 'Фамилия:', surname)
+#                                                               Можно использовать разные способы.
+# name = 'Timur'
+# surname = 'Guev'
+# print('Имя:', name, 'Фамилия:', surname)
+#                                                                       С переменной.
+# name, surname = input(), input()
+# print('Имя:', name, 'Фамилия:', surname)
+# --------------------------------------------------------------
+#                           #Пример
+# Примечание 3. В качестве названия переменных запрещено использовать ключевые (зарезервированные) слова. К ключевым словам в языке Python относятся:
+
+# False;
+# True;
+# None;
+# and;
+# with;
+# as;
+# assert;
+# break;
+# class;
+# continue;
+# def;
+# del;
+# elif;
+# else;
+# except;
+# finally;
+# try;
+# for;
+# from;
+# global;
+# if;
+# import;
+# in;
+# is;
+# lambda;
+# nonlocal;
+# not;
+# or;
+# pass;
+# raise;
+# return;
+# while;
+# yield.
+
+# a = 3
+# b = 2
+
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+
+# Для того чтобы преобразовать строку к целому числу, мы используем команду int().
+# num1 = input()
+# num2 = input()
+
+# print(num1 + num2)
+
+# num1 = int(input())
+# num2 = int(input())
+
+# print(num1 + num2)
+
+# num = 17
+# s = str(num)      # теперь в переменной s содержится строка '17'
